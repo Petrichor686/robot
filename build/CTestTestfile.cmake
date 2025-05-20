ff95c3ae36a3e0dead4e6c5852557eddbcb61b97
Petrichor686 <wqf3517609762@qq.com>
2025-05-20 21:52:18 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/petrichor/robot/src
+# Build directory: /home/petrichor/robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
